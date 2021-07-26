043850a2861741f6391993bac498a0ab3437b06a
wilian1808 <wilian.walli@gmail.com>
2021-07-25 23:06:33 -0500

    add animation banne

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -3,16 +3,22 @@ import './index.css'
 import LoginForm from 'components/LoginForm'
 import Slogan from 'components/slogan'
 
+import { useState } from 'react'
+
 export default function Banner ({ header }) {
-  console.log('la altura es: ', header)
+  const [load, setLoad] = useState(false)
+
+  setTimeout(() => {
+    setLoad(true)
+  }, 100)
 
   return (
     <div className="banner">
       <div className="banner__content">
-        <div className="banner__description">
+        <div className={`banner__description ${load ? 'bannerd__move' : ''}`}>
           <Slogan></Slogan>
         </div>
-        <div className="banner__login">
+        <div className={`banner__login ${load ? 'bannerl__move' : ''}`}>
           <LoginForm></LoginForm>
         </div>
       </div>
